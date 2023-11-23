8c7d84fd0ef75643980fafc9f0f4af31988ecc6d
alec <alec@neon.tech>
2023-11-23 12:49:10 +0000

    rewrite userData

--- src/main/java/neon/Provider.java
+++ src/main/java/neon/Provider.java
@@ -5,15 +5,13 @@ import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.storage.StorageId;
 import org.keycloak.storage.UserStorageProvider;
 import org.keycloak.storage.user.UserLookupProvider;
 
+
 import java.sql.*;
 
-/**
- * Hello world!
- *
- */
 public class Provider implements
         UserStorageProvider,
         UserLookupProvider
@@ -34,11 +32,26 @@ public class Provider implements
 
     @Override
     public UserModel getUserById(RealmModel realm, String s) {
+        System.out.println("GET BY ID");
         try {
-            PreparedStatement stmt = conn.prepareStatement("select * from users where id = ?");
-            stmt.setString(1, s);
+            StorageId id = new StorageId(s);
+            PreparedStatement stmt = conn.prepareStatement("select * from users where id = ?::uuid");
+            stmt.setString(1, id.getExternalId());
+            ResultSet results = stmt.executeQuery();
 
-            return userFromResultSet(realm, stmt.executeQuery());
+            // go to the first result- or, if there are no results, return null to indicate there is no such user
+            if (!results.next()) {
+                return null;
+            }
+
+            UserModel user = userFromResultSet(realm, results);
+
+            // check for further results- if there are, throw an error
+            if (results.next()) {
+                throw new RuntimeException("multiple results from user lookup query");
+            }
+
+            return user;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -46,11 +59,25 @@ public class Provider implements
 
     @Override
     public UserModel getUserByEmail(RealmModel realm, String s) {
+        System.out.println("GET BY EMAIL");
         try {
             PreparedStatement stmt = conn.prepareStatement("select * from users where email = ?");
             stmt.setString(1, s);
+            ResultSet results = stmt.executeQuery();
 
-            return userFromResultSet(realm, stmt.executeQuery());
+            // go to the first result- or, if there are no results, return null to indicate there is no such user
+            if (!results.next()) {
+                return null;
+            }
+
+            UserModel user = userFromResultSet(realm, results);
+
+            // check for further results- if there are, throw an error
+            if (results.next()) {
+                throw new RuntimeException("multiple results from user lookup query");
+            }
+
+            return user;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -58,25 +85,19 @@ public class Provider implements
 
     @Override
     public UserModel getUserByUsername(RealmModel realm, String s) {
+        System.out.println("GET BY USERNAME");
         return getUserByEmail(realm, s); // usernames are emails in our system
     }
     
     private UserModel userFromResultSet(RealmModel realm, ResultSet results) throws SQLException {
-        if (!results.next()) {
-            return null;
-        }
+        return new UserData(
+            session, realm, model,
 
-        UserData user = new UserData(session, realm, model);
-
-        user.setId(results.getString("id"));
-        user.setEmail(results.getString("email"));
-        user.setFirstName(results.getString("name"));
-        user.setLastName(results.getString("last_name"));
-
-        if (results.next()) {
-            throw new RuntimeException("multiple results from user lookup query");
-        }
-
-        return user;
+            results.getString("id"),
+            results.getString("email"),
+            results.getString("name"),
+            results.getString("last_name"),
+            results.getString("deleted_at") != null
+        );
     }
 }
--- src/main/java/neon/model/UserData.java
+++ src/main/java/neon/model/UserData.java
@@ -2,44 +2,54 @@ package neon.model;
 
 import org.keycloak.component.ComponentModel;
 import org.keycloak.credential.LegacyUserCredentialManager;
-import org.keycloak.models.*;
-import org.keycloak.storage.adapter.AbstractUserAdapter;
+import org.keycloak.models.ClientModel;
+import org.keycloak.models.GroupModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.RoleModel;
+import org.keycloak.models.SubjectCredentialManager;
+import org.keycloak.models.UserModel;
+import org.keycloak.storage.ReadOnlyException;
+import org.keycloak.storage.StorageId;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 
-public class UserData extends AbstractUserAdapter implements UserModel {
+public class UserData implements UserModel {
     private final KeycloakSession session;
     private final RealmModel realm;
+    private final ComponentModel model;
 
-    private String id, email, firstName, lastName;
+    private final String id;
+    private final String email;
+    private final String firstName;
+    private final String lastName;
+    private final boolean enabled;
 
-    public UserData(KeycloakSession session, RealmModel realm, ComponentModel model) {
-        super(session, realm, model);
+    public UserData(
+        KeycloakSession session, RealmModel realm, ComponentModel model,
+
+        String id,
+        String email,
+        String firstName,
+        String lastName,
+        boolean enabled
+    ) {
         this.session = session;
         this.realm = realm;
+        this.model = model;
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.enabled = enabled;
     }
 
     @Override
     public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
+        return new StorageId(model.getId(), id).getId();
     }
 
     @Override
@@ -48,22 +58,203 @@ public class UserData extends AbstractUserAdapter implements UserModel {
     }
 
     @Override
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Override
     public String getLastName() {
         return lastName;
     }
 
     @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean isEmailVerified() {
+        return false;
+    }
+
+    @Override
+    public Long getCreatedTimestamp() {
+        return null;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+
+
+    @Override
+    public String getFederationLink() {
+        return null;
+    }
+
+    @Override
+    public Stream<GroupModel> getGroupsStream() {
+        return Stream.empty();
+    }
+
+    @Override
+    public String getServiceAccountClientLink() {
+        return null;
+    }
+
+
+
+    @Override
+    public void setEmail(String email) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public void setEmailVerified(boolean verified) {
+        throw new ReadOnlyException();
+    }
+
+
+    @Override
+    public void joinGroup(GroupModel group) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public void leaveGroup(GroupModel group) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public boolean isMemberOf(GroupModel group) {
+        return false;
+    }
+
+
+
+    @Override
+    public void setFederationLink(String link) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public void setServiceAccountClientLink(String clientInternalId) {
+        throw new ReadOnlyException();
+
+    }
+
+    @Override
+    public void setUsername(String username) {
+        throw new ReadOnlyException();
+
+    }
+
+    @Override
+    public void setCreatedTimestamp(Long timestamp) {
+        throw new ReadOnlyException();
+
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        throw new ReadOnlyException();
+
+    }
+
+    @Override
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        throw new ReadOnlyException();
+
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        throw new ReadOnlyException();
+    }
+
+
+
+    @Override
+    public void setSingleAttribute(String name, String value) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public void setAttribute(String name, List<String> values) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public String getFirstAttribute(String name) {
+        return null;
+    }
+
+    @Override
+    public Stream<String> getAttributeStream(String name) {
+        return Stream.empty();
+    }
+
+    @Override
+    public Map<String, List<String>> getAttributes() {
+        return null;
+    }
+
+
+
+    @Override
+    public Stream<String> getRequiredActionsStream() {
+        return Stream.empty();
+    }
+
+    @Override
+    public void addRequiredAction(String action) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public void removeRequiredAction(String action) {
+        throw new ReadOnlyException();
     }
 
     @Override
     public SubjectCredentialManager credentialManager() {
         return new LegacyUserCredentialManager(session, realm, this);
+    }
+
+    @Override
+    public Stream<RoleModel> getRealmRoleMappingsStream() {
+        return Stream.empty();
+    }
+
+    @Override
+    public Stream<RoleModel> getClientRoleMappingsStream(ClientModel app) {
+        return Stream.empty();
+    }
+
+    @Override
+    public boolean hasRole(RoleModel role) {
+        return false;
+    }
+
+    @Override
+    public Stream<RoleModel> getRoleMappingsStream() {
+        return null;
+    }
+
+    @Override
+    public void grantRole(RoleModel role) {
+        throw new ReadOnlyException();
+    }
+
+    @Override
+    public void deleteRoleMapping(RoleModel role) {
+        throw new ReadOnlyException();
     }
 }
